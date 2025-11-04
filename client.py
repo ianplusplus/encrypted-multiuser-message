5e844adc284176e9e147043e2ed644082ad7d07d
ianplusplus <iangessler@gmail.com>
2025-11-03 20:04:17 -0600

    Added arguments to the clients file.

--- client.py
+++ client.py
@@ -1,14 +1,25 @@
 import socket
+import argparse
 from security import encrypt, decrypt
 
-HOST = '192.168.2.15'  # Server IP
-PORT = 6767        # Server port
+parser = argparse.ArgumentParser(description="Port-Message Client")
+
+parser.add_argument("--address", "-a", help="Address of the server.")
+parser.add_argument("--port", "-p", type=int, default=6767, help="Listening port for server.")
+parser.add_argument("--secret", "-s", help="Sets the password")
+args = parser.parse_args()
+
+HOST = args.address  # Server IP
+PORT = args.port        # Server port
 
 # Create TCP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))  # Connect to server
 
-password = input("Enter password: ")
+if args.secret == None:
+    password = input("Enter password: ")
+else:
+    password = args.secret
 
 print("Enter in messages. Press enter to send message. Send ':end' to end communication.")
 
