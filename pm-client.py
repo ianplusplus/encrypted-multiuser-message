4e863b0c99bf4ff1d35a258f824c10ed5df3a792
ianplusplus <iangessler@gmail.com>
2025-11-04 06:17:38 -0600

    Cleaned up old files and moved the variable length messages logic to it's own .py file to avoid repeated  logic

--- pm-client.py
+++ pm-client.py
@@ -1,33 +1,10 @@
 import socket
 import argparse
 from security import encrypt, decrypt
+from messages import recv_message, send_message
 import threading
 
 running = True
-
-def send_message(sock, text):
-    data = text.encode('utf-8')
-    length = len(data).to_bytes(4, 'big')  # 4-byte length prefix
-    sock.sendall(length + data)
-
-def recv_message(sock):
-    # Receive the 4-byte size prefix
-    length_bytes = sock.recv(4)
-    if not length_bytes:
-        return None
-
-    length = int.from_bytes(length_bytes, 'big')
-
-    # Read exact number of bytes
-    data = b''
-    while len(data) < length:
-        packet = sock.recv(length - len(data))
-        if not packet:
-            return None
-        data += packet
-
-    return data.decode('utf-8')
-
 
 def receive(sock, passwd):
     while True:
