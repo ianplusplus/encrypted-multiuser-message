78857a70b002933a9425b92a4bcb6305b1295284
ianplusplus <iangessler@gmail.com>
2025-11-06 19:17:53 -0600

    Added logic to load public and private key to akey as well as check for a file existence. The program will now create a key if it does not detect the one it would need.

--- akey.py
+++ akey.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import getpass
+from cryptography.hazmat.primitives import serialization
 
 def generate_encrypted_ed25519_keypair(
     private_key_path="client_private_ed25519.pem",
@@ -28,3 +29,28 @@ def generate_encrypted_ed25519_keypair(
 
     os.chmod(private_key_path, 0o600)
     print(f"Encrypted Ed25519 keypair created: {private_key_path} & {public_key_path}")
+
+def load_private_key(private_path="client_private.pem"):
+    with open(private_path, "rb") as key_file:
+        encrypted_key = key_file.read()
+    
+    passphrase = getpass.getpass("Enter private key passphrase: ")
+
+    private_key = serialization.load_pem_private_key(
+        encrypted_key,
+        password=passphrase.encode("utf-8")
+    )
+    print(f"Private key loaded: {private_path}")
+    return private_key
+
+def load_public_key(public_path="client_public.pem"):
+    with open(public_path, "rb") as key_file:
+        public_key_data = key_file.read()
+    print(f"Public key loaded: {public_path}")
+    return public_key_data  # we return raw PEM bytes here
+
+def file_exists(path):
+    if os.path.exists(path):
+        return True
+    else:
+        return False
